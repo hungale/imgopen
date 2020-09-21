54c4b8b1fbd335e2639466a99aec39bd3f333580
A H <10256335+hungale@users.noreply.github.com>
2020-09-20 17:30:31 -0700

    add documentation, remove excess comments

--- imgopenttk.py
+++ imgopenttk.py
@@ -6,6 +6,8 @@ import os
 import sys
 from tkinter import Label, TOP, LEFT, Tk
 from tkinter import ttk
+from shutil import move
+
 
 # from tkinter import tk
 from tkinter import filedialog, messagebox, Message, Toplevel, BOTTOM
@@ -13,55 +15,57 @@ from tkinter import filedialog, messagebox, Message, Toplevel, BOTTOM
 
 class ImageOpener:
     """
-    ImageOpener class.
     Container for all the GUI parts.
     """
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, root, *args, **kwargs):
         # set self.master to root
-        self.master = master
-        master.title("Basic Image Opener")
-        # btn.pack()
+        self.master = root
 
-        # settings
-        master.configure(background="#73afa7")
+        # configure settings
+        # name and UI related
+        self.master.title("imgopen")
+        self.master.configure(background="#73afa7")
         self.framewidth, self.frameheight = (600, 600)
         self.windowwidth, self.windowheight = (1000, 800)
-        root.geometry("%dx%d" % (self.windowwidth, self.windowheight))
+        self.master.geometry("%dx%d" % (self.windowwidth, self.windowheight))
+
+        # options related
         self.ext = ("jpg", "png")
         self.debug = False  # kwargs.get('debug', False)
         self.permanent_delete = False
         self.show_info = False
-        self.buttonWidth = 10
+        self.buttonWidth = 10  # size of button
 
         # find a folder of images and generate a list of the images in them
         self.set_dir_name()
         self.generate_img_list()
 
-        # image goes on label
+        # image goes on label area
         self.image_label = Label(compound=TOP, bg="#eee", borderwidth=2)
         self.image_label.pack()
         self.image_label.place(x=90, y=90)
         self.current = 0
 
-        # ll = info, lr = page #
+        # lower left (ll) = info, lower right (lr) = page number
         self.ll = ttk.Label(self.master, text="lower left")
         self.ll.place(relx=0.0, rely=1.0, anchor="sw")
         self.lr = ttk.Label(self.master, text="lower right")
         self.lr.place(relx=1.0, rely=1.0, anchor="se")
 
         # nav bar goes on frame
-        # , width=600, height=600, borderwidth = 1)
-        self.frame = ttk.Frame(master)
+        self.frame = ttk.Frame(self.master)
         self.frame.pack()
 
         # generate the nav bar
         self.create_nav_bar()
 
+        # go to the first image in the list
         self.move(0)
 
-    # allows you to navigate through the image list forwards or backwards
     def move(self, delta):
-        # from: https://stackoverflow.com/questions/46359590/tkinter-update-image-list
+        """ allows you to navigate through the image list forwards or backwards
+        from: https://stackoverflow.com/questions/46359590/tkinter-update-image-list
+        """
         if self.debug:
             print(
                 (
@@ -71,22 +75,10 @@ class ImageOpener:
                     len(self.sorted_imagelist),
                 )
             )
+
+        # boundary check, makes sure that there are still images in the list
         if not (0 <= self.current + delta < len(self.sorted_imagelist)):
-            #
-            # top = Toplevel(root)
-            # top.title('End')
-            # Message(top, text='No more images.', padx=20, pady=20).pack(BOTTOM)
-            # top.after(2000, top.destroy)
-            # self.show_info = 'No more images.'
-            # body = 'No more images'a
-            if sys.platform == "darwin":
-                messagebox.showerror(title="End", message="No more images.")
-                # messagebox.showerror(title="Invalid Steam folder!",
-                #                  message="{0} is not a valid Steam folder.".format("nuddin"))
-                # os.system("osascript -e 'Tell application \"System Events\" to display dialog \""+body+"\"'")
-            else:
-                messagebox.showerror(title="End", message="No more images.")
-                # messagebox.showinfo("End", "No more images.")
+            messagebox.showerror(title="End", message="No more images.")
             self.master.update()
             return
         self.current += delta
@@ -94,7 +86,7 @@ class ImageOpener:
         # set page number
         self.lr.configure(text=str(self.current))
 
-        # open image and  find width+height
+        # open image and find width+height
         image = PIL.Image.open(self.sorted_imagelist[self.current])
         width, height = image.size
 
@@ -114,12 +106,13 @@ class ImageOpener:
         # center the image
         self.image_label.pack()
 
-    # resizes the image to fit within the framewidth
     def resizer(self, imgWidth, imgHeight, wWidth, wHeight):
+        """ resizes the image to fit within the framewidth"""
         # get the ratio to find the factor
         relWidth = imgWidth / wWidth
         relHeight = imgHeight / wHeight
 
+        # calculate the ratio between image display area size and raw image
         newWidth, newHeight = 0, 0
         if relWidth > relHeight:
             newHeight = round(imgHeight / relWidth)
@@ -129,10 +122,13 @@ class ImageOpener:
             newHeight = wHeight
         if self.debug:
             print(imgWidth, imgHeight, relWidth, relHeight, newWidth, newHeight)
+        
         return newWidth, newHeight
 
     def create_nav_bar(self):
-        # super outside lambda: super().reload(), inside lambda: lambda:super(navigationBar, self).reload())
+        """ create a nav bar at the top of the app
+        note: super outside lambda: super().reload(), inside lambda: lambda:super(navigationBar, self).reload())
+        """
         buttons = [  # txt, h, w, callback
             ("Reload", self.buttonWidth, self.reload),
             ("Info", self.buttonWidth, lambda: self.toggle_info(True)),
@@ -148,16 +144,17 @@ class ImageOpener:
             self.create_button(*button)
 
     def create_button(self, txt, w, callback):
+        """ create the buttons that go onto the navbar, uses templated text"""
         btn = ttk.Button(self.frame, text=f"\n{txt}\n", width=w, command=callback)
         btn.pack(side=LEFT)
 
-    # returns the filename for the current image being shown
     def get_curr_img_name(self):
+        """ returns the filename for the current image being shown"""
         return str(self.sorted_imagelist[self.current])
 
-    # select the folder to browse
     # this code gives an error, text input context does not correspond
     def set_dir_name(self):
+        """ select the folder to browse images in"""
         self.dir_name = filedialog.askdirectory()
         # parent=self.master, initialdir="~", title='Please select a directory')
         if len(self.dir_name) > 0:
@@ -166,7 +163,11 @@ class ImageOpener:
             self.master.quit()
 
     def generate_img_list(self):
-        # walk generates 3 iterables: [0] is name of folder, [1] is names of sub folder, [2] is name of files
+        """ walk generates 3 iterables:
+            [0] is name of folder,
+            [1] is names of sub folder,
+            [2] is name of files
+        """
         self.image_list = [
             os.path.join(self.dir_name, img).replace("\\", "/")
             for img in next(os.walk(self.dir_name))[2]
@@ -179,8 +180,8 @@ class ImageOpener:
         if self.debug:
             print("sorted imagelist:", self.sorted_imagelist)
 
-    # choose a different folder to reload into
     def reload(self):
+        """ choose a different folder to reload into"""
         # hide the imageOpener
         self.master.withdraw()
 
@@ -191,22 +192,25 @@ class ImageOpener:
         # unhide the imageOpener
         self.master.deiconify()
 
+        # TODO: make the reopened imgopener float to the top
+
         # move to the first picture of the newly loaded image list
         self.move(0)
 
-    # moves the current image to a new folder, makes a folder if it doesn't exist
     def move_file(self, file, new_folder):
-        from shutil import move
-
+        """ moves the current image to a new folder,
+        makes a folder if it doesn't exist"""
         dir_path = self.dir_name + "/" + new_folder
+
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
         if os.path.exists(dir_path):
             move(file, dir_path)
 
     def delete_image(self):
-        # soft delete
+        """ soft deletes and image by moving to `delete` folder"""
         file = self.get_curr_img_name()
+
         if not self.permanent_delete:
             self.move_file(file, "delete")
             self.sorted_imagelist.remove(file)
@@ -221,22 +225,29 @@ class ImageOpener:
             os.remove(file)
 
     def keep_file(self):
+        """ moves the current image to `keep` folder"""
         file = self.get_curr_img_name()
         self.move_file(file, "keep")
         self.sorted_imagelist.remove(file)
-        self.move(0)  # update the shown image
+
+        # update the shown image
+        self.move(0)
 
     def open_current_folder(self):
+        """ opens the folder containing the current image
+        TODO: move this into another file (one for mac, one for windows)
+        """
+        # for mac
         if sys.platform == "darwin":
             os.system("open " + self.dir_name)
-        else:
+        else:  # for windows
             os.system(f"explorer {self.dir_name}")
             # os.startfile(self.dir_name)
 
-    # flips the show_info flag if user desires, toggles the info label on/off
-
     def toggle_info(self, flip):
-        # flip the flag if you want it to be
+        """flips the show_info flag if user desires,
+        toggles the info label (lower left corner) on/off"""
+        # flip the flag
         if flip:
             self.show_info = not self.show_info
 
@@ -247,6 +258,7 @@ class ImageOpener:
             self.ll.configure(text="")
 
 
+# main loop, move to another file 
 if __name__ == "__main__":
     root = Tk()
     ImageOpener(root)
